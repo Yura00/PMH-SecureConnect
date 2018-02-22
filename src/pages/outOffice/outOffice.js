0470b9076b4eec57c05d84731704f1a99676f538
Yura <yura.dev00@gmail.com>
2018-02-22 23:32:24 +0800

    Out of Office screen

--- src/pages/outOffice/outOffice.js
+++ src/pages/outOffice/outOffice.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
-import { View, Text, ListView } from 'react-native';
-import { TopBar, ChatListItem, Switch } from '../../components'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-elements'
+import { TopBar } from '../../components'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import styles from './outOffice.style'
-
-import ActionButton from 'react-native-action-button'
-import data from '../mockData'
 
 class OutOffice  extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows(data),
-    };
   }
 
   render() {
     return (
-        <View style={styles.container}>
-            <TopBar onDrawer navigation={this.props.navigation}>
-                <Text style={styles.title}> Out of Office </Text>
-            </TopBar>
-            
+      <View style={styles.container}>
+        <TopBar onDrawer navigation={this.props.navigation}>
+            <Text style={styles.title}> Out of Office </Text>
+        </TopBar>
+
+        <View style={styles.mainContainer}>
+          <TouchableOpacity style={styles.chooseContact}>
+            <Text style={styles.text}> From </Text>
+            <Text style={styles.text}> 02-12-2018 04:29 PM </Text>
+            <MaterialCommunityIcons name='chevron-right' size={25} color={'gray'} />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.chooseContact}>
+            <Text style={styles.text}> To </Text>
+            <Text style={styles.text}> 02-12-2018 04:29 PM </Text>
+            <MaterialCommunityIcons name='chevron-right' size={25} color={'gray'} />
+          </TouchableOpacity>
+          <Text style={styles.commentTitle}> Message </Text>
+          <TextInput style={styles.commentText} multiline placeholder='Enter your out of office message...'/>
         </View>
+
+        <View style={styles.footerContainer}>
+          <Button style={styles.button} large title='Save' />
+        </View>
+      </View>
     );
   }
 }
