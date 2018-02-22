04e7acd26f3fdf4e423352f39bbe67a126dbdeb6
Yura <yura.dev00@gmail.com>
2018-02-23 00:43:01 +0800

    Support screen

--- src/pages/support/support.js
+++ src/pages/support/support.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
-import { View, Text, ListView } from 'react-native';
-import { TopBar, ChatListItem, Switch } from '../../components'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Button } from 'react-native-elements'
+import { TopBar } from '../../components'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import styles from './support.style'
-
-import ActionButton from 'react-native-action-button'
-import data from '../mockData'
 
 class Support  extends Component {
   constructor(props) {
     super(props);
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
     this.state = {
-      dataSource: ds.cloneWithRows(data),
-    };
+      subject: ''
+    }
   }
 
   render() {
     return (
-        <View style={styles.container}>
-            <TopBar onDrawer navigation={this.props.navigation}>
-                <Text style={styles.title}> Support </Text>
-            </TopBar>
-            
+      <View style={styles.container}>
+        <TopBar onDrawer navigation={this.props.navigation}>
+            <Text style={styles.title}> Support </Text>
+        </TopBar>
+
+        <View style={styles.mainContainer}>
+          <View style={styles.listItem}>
+            <Text style={styles.itemTitle}> Subject </Text>
+            <TextInput style={styles.itemValue} value={this.state.subject} onChangeText={(subject) => this.setState({subject: subject})}/>
+          </View>
+          <TouchableOpacity style={styles.chooseContact}>
+            <Text style={styles.text}> Priority </Text>
+            <MaterialCommunityIcons name='chevron-right' size={25} color={'gray'} />
+          </TouchableOpacity>
+          <Text style={styles.commentTitle}> Message </Text>
+          <TextInput style={styles.commentText} multiline placeholder='Enter your out of office message...'/>
+          <TouchableOpacity style={styles.chooseContact}>
+            <Text style={styles.text}> Attachment </Text>
+          </TouchableOpacity>
         </View>
+
+        <View style={styles.footerContainer}>
+          <Button style={styles.button} large title='Submit' />
+        </View>
+      </View>
     );
   }
 }
